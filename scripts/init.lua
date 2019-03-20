da02fbe41ff5b9980c0333822b3b2c892f8fd530
Nagare86 <jikannotorento@gmail.com>
2019-03-19 19:21:39 -0600

    First Commit
    V1.0.0.0 of the tracker

---
+++ scripts/init.lua
@@ -0,0 +1,6 @@
+Tracker:AddItems("items/bosses.json")
+Tracker:AddItems("items/tanks.json")
+Tracker:AddItems("items/unique.json")
+
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
