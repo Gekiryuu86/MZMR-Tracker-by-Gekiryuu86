0ed06d2338f04249d03d18d1f02adce4db15f4a2
Nagare86 <jikannotorento@gmail.com>
2019-03-22 14:49:18 -0600

    Bingo
    Added bingo variants and restructured

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,18 @@
 Tracker:AddItems("items/bosses.json")
-Tracker:AddItems("items/tanks.json")
 Tracker:AddItems("items/unique.json")
+Tracker:AddItems("items/bingo.json")
+if (string.find(Tracker.ActiveVariantUID, "hard")) then
+    Tracker:AddItems("items/tanks_hard.json")
+  else
+    Tracker:AddItems("items/tanks_normal.json")
+end
 
-Tracker:AddLayouts("layouts/tracker.json")
-Tracker:AddLayouts("layouts/broadcast.json")
+Tracker:AddLayouts("layouts/shared/bingo.json")
+Tracker:AddLayouts("layouts/shared/items.json")
+Tracker:AddLayouts("layouts/shared/bosses.json")
+
+if (string.find(Tracker.ActiveVariantUID, "bingo")) then
+    Tracker:AddLayouts("layouts/tracker_bingo.json")
+  else
+    Tracker:AddLayouts("layouts/tracker.json")
+end
