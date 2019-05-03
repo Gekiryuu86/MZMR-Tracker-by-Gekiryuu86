19bae408f8f8daff39db85fa19bccf94982f6d28
Gekiryuu86 <jikannotorento@gmail.com>
2019-05-03 10:39:38 -0600

    reverted/renamed layouts

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,17 +1,16 @@
 Tracker:AddItems("items/bosses.json")
 Tracker:AddItems("items/unique.json")
-Tracker:AddItems("items/bingo.json")
+
 if (string.find(Tracker.ActiveVariantUID, "hard")) then
     Tracker:AddItems("items/tanks_hard.json")
   else
     Tracker:AddItems("items/tanks_normal.json")
 end
 
-Tracker:AddLayouts("layouts/shared/bingo.json")
 Tracker:AddLayouts("layouts/shared/items.json")
 Tracker:AddLayouts("layouts/shared/bosses.json")
-Tracker:AddLayouts("layouts/shared/layout_default.json")
+Tracker:AddLayouts("layouts/shared/shared_grid.json")
 
-Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/layout.json")
 
 ScriptHost:LoadScript("scripts/autotracking.lua")
